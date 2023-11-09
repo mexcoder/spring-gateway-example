48a61a339a6c7422ca2a044535df9ed8e503d7b6
Juan <iam@mexcoder.com>
2023-11-09 17:15:37 -0600

    spring cloud gateway  minimal demo

---
+++ gateway/src/main/java/com/example/gateway/gateway/GatewayApplication.java
@@ -0,0 +1,19 @@
+package com.example.gateway.gateway;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+
+import java.util.Map;
+import java.util.Objects;
+
+@SpringBootApplication
+public class GatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GatewayApplication.class, args);
+	}
+}
